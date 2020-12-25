96383a4631d4767885d380901ac3ce907fc466d8
Prahalad Ramakrishnan <45518327+ProPra7000@users.noreply.github.com>
2020-12-24 22:57:51 -0500

    Add files via upload

---
+++ ABCs.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+x = list(map(int, input().split()))
+for i in permutations(x):
+    if (i[0] + i[1] == i[3] and i[0] + i[2] == i[4] and i[1] + i[2] == i[5] and i[0] + i[1] + i[2] == i[6]):
+        if (i[0] <= i[1] and i[1] <= i[2]):
+            print(str(i[0]) + ' ' + str(i[1]) + ' ' + str(i[2]))
+            break
